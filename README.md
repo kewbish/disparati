705c2ba93212eed76a350ca5f99dfd7da22de4f9
Emilie Ma <kewbish@gmail.com>
2023-05-13 11:14:35 -0700

    Add snowflake format explanation

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # disparati
 
-Solutions to [Fly.io](https://fly.io)'s [Gossip Glomers](https://fly.io/dist-sys/) (distributed systems) challenges.  
+Solutions to [Fly.io](https://fly.io)'s [Gossip Glomers](https://fly.io/dist-sys/) (distributed systems) challenges. 'Disparati' is a portmanteau of 'distributed apparati'.  
 Written in Go, May 2023 to present.  
 Released under the [MIT License](./LICENSE).  
 Created by [Kewbish](https://github.com/kewbish).
--- unique-id-generation/main.go
+++ unique-id-generation/main.go
@@ -11,17 +11,16 @@ import (
 )
 
 var (
-	EPOCH       int64 = 1616253079084
-	MS_PER_HOUR int64 = 3600000
-	lastTime    int64 = 0
-	counter     int   = 0
+	EPOCH    int64 = 1616253079084
+	lastTime int64 = 0
+	counter  int   = 0
 )
 
 func main() {
 	n := maelstrom.NewNode()
 
 	n.Handle("generate", func(msg maelstrom.Message) error {
-		var body map[string]any = make(map[string]any)
+		// inspired by https://en.wikipedia.org/wiki/Snowflake_ID
 		currentTime := time.Now().UnixMilli()
 		diff := currentTime - EPOCH
 		if currentTime == lastTime {
@@ -29,15 +28,20 @@ func main() {
 		} else {
 			counter = 0
 		}
+		lastTime = currentTime
+
 		idSlice := []byte(n.ID())
 		idInt := binary.BigEndian.Uint32(idSlice[:4])
+		// ensures min / max lengths, 0 padded
+		diffS := fmt.Sprintf("%064s", fmt.Sprintf("%.64b", diff))       // allocate 64 bits, current times use ~59
+		idS := fmt.Sprintf("%032s", fmt.Sprintf("%.32b", idInt))        // allocate 32 bits, will use all
+		counterS := fmt.Sprintf("%032s", fmt.Sprintf("%.32b", counter)) // allocate 32 bits, can handle load of 2^32 generations per ms
+
 		bi := big.NewInt(0)
-		diffS := fmt.Sprintf("%064s", fmt.Sprintf("%.64b", diff))
-		idS := fmt.Sprintf("%032s", fmt.Sprintf("%.32b", idInt))
-		counterS := fmt.Sprintf("%032s", fmt.Sprintf("%.32b", counter))
 		bi.SetString(fmt.Sprintf("%s%s%s", diffS, idS, counterS), 2)
-		snowflake := fmt.Sprintf("%x", bi)
-		lastTime = currentTime
+		snowflake := fmt.Sprintf("%x", bi) // hex encode
+
+		var body map[string]any = make(map[string]any)
 		body["type"] = "generate_ok"
 		body["id"] = snowflake
 		return n.Reply(msg, body)
@@ -46,15 +50,4 @@ func main() {
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
 	}
-
-	/*currentTime := time.Now().UnixMilli()
-	diff := currentTime - EPOCH
-	if currentTime == lastTime {
-		counter++
-	} else {
-		counter = 0
-	}
-	idInt, _ := strconv.ParseUint("0xdeadbeef", 0, 64)
-	snowflake := (diff&int64(math.Pow(2, 42)-1))<<22 | (int64(idInt)&int64(math.Pow(2, 11)-1))<<12 | int64(counter)&int64(math.Pow(2, 13)-1)
-	fmt.Printf("%b %b %b %b", snowflake, diff, idInt, counter)*/
 }
