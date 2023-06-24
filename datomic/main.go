3ff9c3ee1cd9fdede1b8e6451d01306348412f63
Emilie Ma <kewbish@gmail.com>
2023-06-24 15:20:14 -0700

    Add (non-high-throughput) datomic shared state

--- datomic/main.go
+++ datomic/main.go
@@ -1,18 +1,17 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"log"
-	"sync"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 func main() {
 	n := maelstrom.NewNode()
-
-	store := make(map[interface{}][]interface{})
-	var valueMutex sync.RWMutex
+	kv := maelstrom.NewLinKV(n)
 
 	n.Handle("txn", func(msg maelstrom.Message) error {
 		var body map[string]any
@@ -21,14 +20,23 @@ func main() {
 		}
 		transaction := body["txn"].([]interface{})
 		new_transaction := make([][]interface{}, 0)
+		current_store_json, err := kv.Read(context.Background(), "root")
+		var current_store map[string][]interface{}
+		if err != nil || current_store_json == nil {
+			current_store = make(map[string][]interface{})
+		} else {
+			if err := json.Unmarshal([]byte(current_store_json.(string)), &current_store); err != nil {
+				current_store = make(map[string][]interface{})
+			}
+		}
 
-		valueMutex.Lock()
 		for _, mop := range transaction {
 			mop := mop
 			mop_interface := mop.([]interface{})
 			action, key, value := mop_interface[0], mop_interface[1], mop_interface[2]
+			skey := fmt.Sprintf("%d", int64(key.(float64)))
 
-			current_value, exists := store[key]
+			current_value, exists := current_store[skey]
 			if !exists {
 				current_value = make([]interface{}, 0)
 			}
@@ -41,13 +49,18 @@ func main() {
 			case "append":
 				fallthrough
 			default:
-				store[key] = append(store[key], value)
+				new_value := make([]interface{}, len(current_value))
+				copy(new_value, current_value)
+				new_value = append(new_value, value)
+				current_store[skey] = new_value
+				log.Default().Print(current_value, new_value, current_store[skey])
 				new_mop[2] = value
 
 			}
 			new_transaction = append(new_transaction, new_mop)
 		}
-		valueMutex.Unlock()
+		new_json, err := json.Marshal(current_store)
+		kv.CompareAndSwap(context.Background(), "root", current_store_json, string(new_json), true)
 
 		body = make(map[string]any)
 		body["type"] = "txn_ok"
