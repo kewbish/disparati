14790b3f103c994f008bfb42c46326b2dac8f72a
Emilie Ma <kewbish@gmail.com>
2023-07-16 20:27:55 -0700

    Fix nil return and thunk appending issues

--- datomic/main.go
+++ datomic/main.go
@@ -31,11 +31,11 @@ func main() {
 		defer cancel()
 		value, err := kv.Read(ctx, "root")
 		if err != nil {
-			return err
+			value = "[]"
 		}
-		thunkMap.fromJSON(n, kv, value.([]byte))
+		thunkMap = thunkMap.fromJSON(n, kv, []byte(value.(string)))
 
-		newThunkMap := Map{n, kv, make(map[any]*Thunk)}
+		newThunkMap := Map{n, kv, thunkMap.internalMap}
 		new_transaction := make([][]interface{}, 0)
 		for _, mop := range transaction {
 			mop := mop
@@ -47,11 +47,22 @@ func main() {
 			new_mop[1] = key
 			switch action {
 			case "r":
-				new_mop[2] = newThunkMap.internalMap[key]
+				if _, ok := newThunkMap.internalMap[key]; ok {
+					new_mop[2] = newThunkMap.internalMap[key].getThunkValue()
+				} else {
+					new_mop[2] = make([]interface{}, 0)
+				}
 			case "append":
-				new_value := make([]interface{}, len(newThunkMap.internalMap[key].value.([]any)))
-				copy(new_value, newThunkMap.internalMap[key].value.([]any))
-				new_value = append(new_value, value)
+				new_mop[2] = value
+				var new_value []interface{}
+				if _, ok := newThunkMap.internalMap[key]; ok {
+					thunkValue := newThunkMap.internalMap[key].getThunkValue()
+					new_value = make([]interface{}, len(thunkValue.([]any)))
+					copy(new_value, thunkValue.([]any))
+					new_value = append(new_value, value)
+				} else {
+					new_value = []interface{}{value}
+				}
 				thunk := newThunk(n, kv, globalIDG.newID(), new_value, false)
 				newInternalMap := make(map[any]*Thunk)
 				for k, v := range newThunkMap.internalMap {
@@ -66,7 +77,7 @@ func main() {
 		newThunkMap.save()
 
 		ctx, cancel_another := context.WithTimeout(context.Background(), timeout)
-		kv.CompareAndSwap(context.Background(), "root", thunkMap.toJSON(), newThunkMap.toJSON(), true) // TODO - add retry
+		kv.CompareAndSwap(context.Background(), "root", value, newThunkMap.toJSON(), true) // TODO - add retry
 		defer cancel_another()
 		mx.Unlock()
 
@@ -88,7 +99,7 @@ type IDGenerator struct {
 	index int64
 }
 
-func (idg IDGenerator) newID() string {
+func (idg *IDGenerator) newID() string {
 	idg.lock.Lock()
 	idg.index += 1
 	idg.lock.Unlock()
@@ -109,21 +120,28 @@ func newThunk(node *maelstrom.Node, kv *maelstrom.KV, id string, value any, save
 	return thunk
 }
 
-func (t Thunk) getThunkValue() any {
+func (t *Thunk) getThunkValue() any {
+	if t.value != nil {
+		return t.value
+	}
 	contextWithTimeout, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 	value, err := t.kv.Read(contextWithTimeout, t.id)
 	if err != nil {
+		log.Default().Print(err)
 		return nil
 	}
 	t.value = value
 	return value
 }
 
-func (t Thunk) saveThunkValue() {
+func (t *Thunk) saveThunkValue() {
+	if t.saved {
+		return
+	}
 	contextWithTimeout, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
-	err := t.kv.Write(contextWithTimeout, t.id, t.value)
+	err := t.kv.Write(contextWithTimeout, t.id, t.getThunkValue())
 	if err == nil {
 		t.saved = true
 	}
@@ -139,21 +157,23 @@ func (Map) fromJSON(node *maelstrom.Node, kv *maelstrom.KV, jsonString []byte) *
 	newMap := new(Map)
 	newMap.node = node
 	newMap.internalMap = make(map[any]*Thunk)
-	var pairs [][]any
-	json.Unmarshal(jsonString, &pairs)
+	pairs := make([][]any, 0)
+	if len(jsonString) != 0 {
+		json.Unmarshal(jsonString, &pairs)
+	}
 	for _, pair := range pairs {
 		newMap.internalMap[pair[0]] = newThunk(node, kv, pair[1].(string), nil, true)
 	}
 	return newMap
 }
 
-func (m Map) toJSON() []byte {
+func (m Map) toJSON() string {
 	pairs := make([][]any, 0)
 	for k, v := range m.internalMap {
 		pairs = append(pairs, []any{k, v.id})
 	}
 	bytes, _ := json.Marshal(pairs)
-	return bytes
+	return string(bytes)
 }
 
 func (m Map) save() {
